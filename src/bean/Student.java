746881780e5df82a8a16d1190787732a762cdb22
Matcha-17 <ooi2372049@stu.o-hara.ac.jp>
2024-06-05 10:36:30 +0900

    Studentのbean

--- src/bean/Student.java
+++ src/bean/Student.java
@@ -3,39 +3,51 @@ package bean;
 // Serializableインターフェースを実装してBeanを作成する
 public class Student implements java.io.Serializable {
 
-	// DBの項目名と同じ名前のprivateなフィールドを定義
-	private int student_id;
-	private String student_name;
-	private int course_id;
-	// その学生に対応したコース名を取得したいためcourseビーンをもたせる
-	private Course course;
+    // DBの項目名と同じ名前のprivateなフィールドを定義
+    private String NO;           // 学生番号
+    private String NAME;         // 学生名
+    private int ENT_YEAR;        // 入学年度
+    private String CLASS_NUM;    // クラス番号
+    private boolean IS_ATTEND;   // 在学中フラグ
+    private String SCHOOL_CD;    // 学校コード
 
-	// ゲッターメソッド キャメルケースはプロパティ名の先頭のみ
-	// student_nameならばgetStudent_id()です。getStudent_Id()やgetStudentId()などではエラーになります。
-	public int getStudent_id() {
-		return student_id;
-	}
-	public String getStudent_name() {
-		return student_name;
-	}
-	public int getCourse_id() {
-		return course_id;
-	}
-	public Course getCourse() {
-		return course;
-	}
+    // ゲッターメソッド
+    public String getNO() {
+        return NO;
+    }
+    public String getNAME() {
+        return NAME;
+    }
+    public int getENT_YEAR() {
+        return ENT_YEAR;
+    }
+    public String getCLASS_NUM() {
+        return CLASS_NUM;
+    }
+    public boolean getIS_ATTEND() {
+        return IS_ATTEND;
+    }
+    public String getSCHOOL_CD() {
+        return SCHOOL_CD;
+    }
 
-	// セッターメソッド
-	public void setStudent_id(int id) {
-		this.student_id=id;
-	}
-	public void setStudent_name(String student_name) {
-		this.student_name=student_name;
-	}
-	public void setCourse_id(int course_id) {
-		this.course_id=course_id;
-	}
-	public void setCourse(Course course) {
-		this.course = course;
-	}
+    // セッターメソッド
+    public void setNO(String NO) {
+        this.NO = NO;
+    }
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+    public void setENT_YEAR(int ENT_YEAR) {
+        this.ENT_YEAR = ENT_YEAR;
+    }
+    public void setCLASS_NUM(String CLASS_NUM) {
+        this.CLASS_NUM = CLASS_NUM;
+    }
+    public void setIS_ATTEND(boolean IS_ATTEND) {
+        this.IS_ATTEND = IS_ATTEND;
+    }
+    public void setSCHOOL_CD(String SCHOOL_CD) {
+        this.SCHOOL_CD = SCHOOL_CD;
+    }
 }
