a2fc02892ca2af2327260c4d95b96dd76a14696e
YukoOkadan <yuum.evol93819@gmail.com>
2024-06-11 10:08:17 +0900

    LoginActionにtool.Actionを継承

--- src/student/LoginAction.java
+++ src/student/LoginAction.java
@@ -3,9 +3,13 @@ package student;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LoginAction {
-    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+import tool.Action;
+
+public class LoginAction extends Action{
+    public String execute
+    	(HttpServletRequest req, HttpServletResponse res
+			) throws Exception {
         // ログインページにリダイレクト
-        req.getRequestDispatcher("/login.jsp").forward(req, res);
+        return "login.jsp";
     }
 }
