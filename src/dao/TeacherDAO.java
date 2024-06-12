8bb3b50412adff4137c1210c484fb17e8e8110c9
YukoOkadan <yuum.evol93819@gmail.com>
2024-06-12 14:31:54 +0900

    TeacherDAOの不要なコメントを削除＆SQL文をTeacherテーブルとSchoolテーブルを結合し取得に変更

--- src/dao/TeacherDAO.java
+++ src/dao/TeacherDAO.java
@@ -9,42 +9,13 @@ import bean.Teacher;
 
 public class TeacherDAO extends DAO {
 
-    // 教師情報を教師IDで取得するメソッド
-//    public Teacher get(String id) throws Exception {
-//        Teacher teacher = null; // 教師オブジェクトを初期化
-//        Connection con = getConnection(); // データベース接続を取得
-//
-//        // 教師IDを条件にしてSQLクエリを準備
-//        PreparedStatement st = con.prepareStatement("SELECT * FROM teacher WHERE ID = ?");
-//        st.setString(1, id); // パラメータに教師IDを設定
-//        ResultSet rs = st.executeQuery(); // クエリを実行して結果セットを取得
-//
-//        if (rs.next()) {
-//            teacher = new Teacher(); // 教師オブジェクトをインスタンス化
-//            teacher.setId(rs.getString("ID")); // 教師IDを設定
-//            teacher.setName(rs.getString("Name")); // 教師名を設定
-//            teacher.setPassword(rs.getString("Password")); // 教師のパスワードを設定
-//
-//            // 学校ビーンをインスタンス化して情報をセット
-//            School school = new School();
-//            school.setCd(rs.getString("CD")); // 学校コードを設定
-//            school.setName(rs.getString("NAME")); // 学校名を設定
-//            teacher.setSchool(school); // 学校情報を教師オブジェクトに設定
-//        }
-//
-//        st.close(); // ステートメントをクローズ
-//        con.close(); // 接続をクローズ
-//
-//        return teacher; // 教師オブジェクトを返す
-//    }
-
     // ログインメソッド
     public Teacher login(String id, String password) throws Exception {
         Teacher teacher = null; // 教師オブジェクトを初期化
         Connection con = getConnection(); // データベース接続を取得
 
         // 教師IDとパスワードを条件にしてSQLクエリを準備
-        PreparedStatement st = con.prepareStatement("SELECT * FROM teacher WHERE ID = ? AND PASSWORD = ?");
+        PreparedStatement st = con.prepareStatement("SELECT * FROM teacher AS T JOIN SCHOOL AS S ON T.SCHOOL_CD = S.CD WHERE ID = ? AND PASSWORD = ?");
         st.setString(1, id); // パラメータに教師IDを設定
         st.setString(2, password); // パラメータにパスワードを設定
         ResultSet rs = st.executeQuery(); // クエリを実行して結果セットを取得
@@ -58,7 +29,7 @@ public class TeacherDAO extends DAO {
             // 学校ビーンをインスタンス化して情報をセット
             School school = new School();
             school.setCd(rs.getString("SCHOOL_CD")); // 学校コードを設定
-            school.setName(rs.getString("NAME")); // 学校名を設定
+            school.setName(rs.getString("SCHOOL_NAME")); // 学校名を設定
             teacher.setSchool(school); // 学校情報を教師オブジェクトに設定
         }
 
@@ -67,24 +38,27 @@ public class TeacherDAO extends DAO {
 
         return teacher; // 教師オブジェクトを返す
     }
-    public Teacher get(String id) throws Exception {
-        Teacher teacher = null;
-        Connection con = getConnection();
 
-        PreparedStatement st = con.prepareStatement("SELECT * FROM teacher WHERE ID = ?");
-        st.setString(1, id);
-        ResultSet rs = st.executeQuery();
+    public Teacher get(String id) throws Exception {
+        Teacher teacher = null; // 教師オブジェクトを初期化
+        Connection con = getConnection(); // データベース接続を取得
+
+        // 教師IDを条件にしてSQLクエリを準備
+        PreparedStatement st = con.prepareStatement("SELECT * FROM TEACHER AS t JOIN SCHOOL AS s ON t.SCHOOL_CD = s.CD WHERE t.id= ?");
+        st.setString(1, id); // パラメータに教師IDを設定
+        ResultSet rs = st.executeQuery(); // クエリを実行して結果セットを取得
 
         if (rs.next()) {
-            teacher = new Teacher();
-            teacher.setId(rs.getString("ID"));
-            teacher.setPassword(rs.getString("PASSWORD"));
-            teacher.setName(rs.getString("NAME"));
+            teacher = new Teacher(); // 教師オブジェクトをインスタンス化
+            teacher.setId(rs.getString("ID")); // 教師IDを設定
+            teacher.setPassword(rs.getString("PASSWORD")); // 教師のパスワードを設定
+            teacher.setName(rs.getString("NAME")); // 教師名を設定
 
             // Schoolオブジェクトの生成と設定
             School school = new School();
-            school.setCd(rs.getString("SCHOOL_CD"));
-            teacher.setSchool(school);
+            school.setCd(rs.getString("SCHOOL_CD")); // 学校コードを設定
+            school.setName(rs.getString("SCHOOL_NAME")); // 学校名を設定
+            teacher.setSchool(school); // 学校情報を教師オブジェクトに設定
         }
         return teacher; // 教師オブジェクトを返す
     }
