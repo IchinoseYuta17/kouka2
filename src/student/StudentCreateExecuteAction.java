04fb5ac1f9019403cba243040b5b897d57b36a21
YukoOkadan <yuum.evol93819@gmail.com>
2024-06-12 14:45:59 +0900

    teacherオブジェクトからschoolオブジェクトを取得し、student.setSchool()するように変更

--- src/student/StudentCreateExecuteAction.java
+++ src/student/StudentCreateExecuteAction.java
@@ -3,10 +3,8 @@ package student;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import bean.School;
 import bean.Student;
 import bean.Teacher;
-import dao.SchoolDAO;
 import dao.StudentDAO;
 import tool.Action;
 
@@ -19,26 +17,19 @@ public class StudentCreateExecuteAction extends Action {
 		String no=request.getParameter("studentNumber");
 		String name=request.getParameter("name");
 		String classNum=request.getParameter("class");
+
 		Boolean isAttend = true;
+
 		Teacher teacher =(Teacher)request.getSession().getAttribute("teacher");
-        School belongSchool = teacher.getSchool();
-        String schoolCd = belongSchool.getCd();
-
-        // 学校オブジェクトを作成し、学校コードを設定
-      	SchoolDAO schoolDAO = new SchoolDAO();
-        School school = new School();
-        school = schoolDAO.get(schoolCd);
-
-
 
 		// Studentビーンに設定
 		Student student=new Student();
-		student.setEntYear(entYear);
 		student.setNo(no);
 		student.setName(name);
+		student.setEntYear(entYear);
 		student.setClassNum(classNum);
 		student.setIsAttend(isAttend);
-		student.setSchool(school);
+		student.setSchool(teacher.getSchool());
 
 		// StudentDAOインスタンスを生成
 		StudentDAO dao=new StudentDAO();
