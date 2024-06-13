0391ecb239c545ad1d94292781244f191fa85441
YukoOkadan <yuum.evol93819@gmail.com>
2024-06-13 09:26:21 +0900

    StudentUpdateActionの作成

--- src/student/StudentUpdateAction.java
+++ src/student/StudentUpdateAction.java
@@ -1,9 +1,14 @@
 package student;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.ClassNum;
 import bean.Student;
+import bean.Teacher;
+import dao.ClassNumDAO;
 import dao.StudentDAO;
 import tool.Action;
 
@@ -11,29 +16,26 @@ public class StudentUpdateAction extends Action {
 	public String execute(
 		HttpServletRequest request, HttpServletResponse response
 	) throws Exception {
-		// リクエストパラメータを受け取る
-		int student_id=Integer.parseInt(request.getParameter("student_id"));
-		String student_name=request.getParameter("student_name");
-		int course_id=Integer.parseInt(request.getParameter("course_id"));
 
-		// StudentBeanの生成
-		Student student=new Student();
-		student.setStudent_id(student_id);
-		student.setStudent_name(student_name);
-		student.setCourse_id(course_id);
+        // セッションから教師情報を取得
+        Teacher teacher = (Teacher) request.getSession().getAttribute("teacher");
+
+		// リクエストパラメータを受け取る
+		String no=request.getParameter("student_no");
+
 
 		// StudentDAOの生成
 		StudentDAO dao=new StudentDAO();
+		Student student = dao.studentGet(no, teacher.getSchool());
 
-		// 学生の更新
-		int line = dao.updateStudent(student);
+		ClassNumDAO classNumDAO = new ClassNumDAO();
+		List<ClassNum> classNumList = classNumDAO.filter(teacher.getSchool());
 
-		// lineが0でなければ更新成功
-		if (line != 0) {
-			request.setAttribute("message", "更新しました");
-		} else {
-			request.setAttribute("message", "更新に失敗しました");
-		}
-		return "result.jsp";
+        // 取得したクラスデータをリクエストに設定
+		request.setAttribute("student", student);
+		request.setAttribute("teacher", teacher);
+		request.setAttribute("classNumList", classNumList);
+
+		return "student_update.jsp";
 	}
 }
