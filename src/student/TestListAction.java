7f0c753844c6c9a1e8c6925f83d98ab664fae2d0
Matcha-17 <ooi2372049@stu.o-hara.ac.jp>
2024-06-19 11:58:35 +0900

    データ取得を記述

--- src/student/TestListAction.java
+++ src/student/TestListAction.java
@@ -1,22 +1,23 @@
 package student;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import bean.School;
+import bean.ClassNum;
 import bean.Student;
 import bean.Subject;
 import bean.Teacher;
 import bean.TestListStudent;
-import bean.TestListSubject;
+import dao.ClassNumDAO;
 import dao.StudentDAO;
 import dao.SubjectDAO;
+import dao.TestDAO;
 import dao.TestListStudentDAO;
-import dao.TestListSubjectDAO;
 import tool.Action;
 import util.Util;
 
@@ -32,11 +33,29 @@ public class TestListAction extends Action {
                 return "login.jsp";
             }
 
+            // 使用するDAOの生成
+    		SubjectDAO subjectDAO = new SubjectDAO();
+    		ClassNumDAO classNumDAO = new ClassNumDAO();
+            TestDAO testDAO = new TestDAO();
+
+
+    		// ユーザーデータをもとにユーザーが所属してる学校のクラスデータを取得
+    		List<ClassNum> classNumList = new ArrayList<>();
+    		classNumList = classNumDAO.filter(teacher.getSchool());
+
+    		// ユーザーデータをもとにユーザーが所属してる学校の科目データを取得
+    		List<Subject> subjectList = new ArrayList<>();
+    		subjectList = subjectDAO.filter(teacher.getSchool());
+
+    		// リクエスト属性にクラスリストと科目リストを設定
+    		req.setAttribute("classNumList", classNumList);
+    		req.setAttribute("subjectList", subjectList);
+
             // リクエストパラメータを取得
             String studentNo = req.getParameter("studentNo");
-            String entYearParam = req.getParameter("entYear");
-            String classNum = req.getParameter("classNum");
-            String subjectCd = req.getParameter("subjectCd");
+//            String entYearParam = req.getParameter("entYear");
+//            String classNum = req.getParameter("classNum");
+//            String subjectCd = req.getParameter("subjectCd");
 
 
 
@@ -65,21 +84,21 @@ public class TestListAction extends Action {
 
 
 
-            // 科目別の処理
-            if (entYearParam != null && !entYearParam.isEmpty() &&
-                classNum != null && !classNum.isEmpty() &&
-                subjectCd != null && !subjectCd.isEmpty()) {
-
-                int entYear = Integer.parseInt(entYearParam);
-                School school = teacher.getSchool();
-                SubjectDAO subjectDAO = new SubjectDAO();
-                Subject subject = subjectDAO.get(subjectCd, school);
-
-                TestListSubjectDAO testListSubjectDAO = new TestListSubjectDAO();
-                List<TestListSubject> testListSubjects = testListSubjectDAO.filter(entYear, classNum, subject, school);
-
-                req.setAttribute("testListSubjects", testListSubjects);
-            }
+//            // 科目別の処理
+//            if (entYearParam != null && !entYearParam.isEmpty() &&
+//                classNum != null && !classNum.isEmpty() &&
+//                subjectCd != null && !subjectCd.isEmpty()) {
+//
+//                int entYear = Integer.parseInt(entYearParam);
+//                School school = teacher.getSchool();
+//                SubjectDAO subjectDAO = new SubjectDAO();
+//                Subject subject = subjectDAO.get(subjectCd, school);
+//
+//                TestListSubjectDAO testListSubjectDAO = new TestListSubjectDAO();
+//                List<TestListSubject> testListSubjects = testListSubjectDAO.filter(entYear, classNum, subject, school);
+//
+//                req.setAttribute("testListSubjects", testListSubjects);
+//            }
 
             // JSPページにフォワード
             return "test_list_student.jsp";
